0b292faf57cba9c94ec42da0f62c12b33e37051b
Sabrina Chapados <takara@Sabrinas-MacBook-Pro.local>
2021-02-03 22:20:50 -0500

    added second client, now it's broken

--- Assignment 1/client.c
+++ Assignment 1/client.c
@@ -27,7 +27,7 @@ int main(int argc, const char * argv[]) {
         int version;
         char source[50];
         char destination[50];
-        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who 
+        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who 5=quit
         char data[256];
     };
     int sock; // socket descriptor
@@ -35,6 +35,11 @@ int main(int argc, const char * argv[]) {
     struct packet input, output;
     char username[50];
     int logged_in = 0;
+
+    // start login request
+    printf("Enter a username => ");
+    scanf("%s", username);
+    input.verb = 1;
 
     // create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -44,39 +49,24 @@ int main(int argc, const char * argv[]) {
 
     // connect to the server
     srv.sin_family = AF_INET; // use internet address family
-    srv.sin_port = htons(1989); // connect to port 1989
+    srv.sin_port = htons(53943); // connect to port 1989
     srv.sin_addr.s_addr = inet_addr("192.197.151.116"); // connect to loki
     if (connect(sock, (struct sockaddr*) &srv, sizeof(srv)) < 0) {
         perror("connect call failed");
         exit(1);
     }
 
-    // send and receive messages
-    while (1) {
-        input.version = 1;
-        if (!logged_in) {
-            // log user in
-            printf("Enter a username => ");
-            fgets(username, 50, stdin);
-            strcpy(input.source, username);
-            input.verb = 1;
-            logged_in = 1;
-            printf("Logging in...\n");
+    
+        strcpy(input.source, username);
+        printf("Message: ");
+        fgets(input.data, 256, stdin);
+
+        // check for exit condition
+        if (strncmp(input.data, "bye", 3) == 0) {
+            printf("Sorry to see you go\n");
+            exit(0);
         }
-        else {
-            printf("Send message to: ");
-            fgets(input.destination, 50, stdin);
-            if (strncmp(input.destination, "bye", 3) == 0) { // end loop
-                printf("Sorry to see you go\n");
-                exit(0);
-            }
-            printf("Message: ");
-            fgets(input.data, 256, stdin);
-            if (strncmp(input.destination, "all", 3) == 0)
-                input.verb = 2;
-            else
-                input.verb = 3;
-        }
+
         send(sock, &input, 512, 0);
         if (recv(sock, &output, 512, 0) > 0)
             printf("%s: %s\n", output.source, output.data);
@@ -84,7 +74,44 @@ int main(int argc, const char * argv[]) {
             printf("Server has died\n");
             close(sock);
             exit(1);
-        } 
-    }
-    return 0;
+        }
+    
+    
+
+
+    // // send login request
+    // write(sock, &input, 512);
+
+    // // send and receive messages
+    // while (read(sock, &output, 512) > 0) {
+    //     // print message from server
+    //     printf("%s: %s\n", output.source, output.data);
+        
+    //     input.version = 1;
+    //     strcpy(input.source, username);
+    //     printf("Send message to: "); // username, all, or bye
+    //     scanf("%s", input.destination);
+
+    //     // check for exit condition
+    //     if (strncmp(input.destination, "bye", 3) == 0) {
+    //         input.verb = 5;
+    //         write(sock, &input, 512); // notify server that user has left
+    //         printf("Sorry to see you go\n");
+    //         exit(0);
+    //     }
+        
+    //     // get message to send
+    //     printf("Message: "); 
+    //     fgets(input.data, 256, stdin);
+    //     if (strncmp(input.destination, "all", 3) == 0)
+    //         input.verb = 2; // message to all
+    //     else
+    //         input.verb = 3; // private message
+        
+    //     // send to server
+    //     write(sock, &input, 512);
+    // }
+    // printf("Server has died\n");
+    // close(sock);
+    // exit(1); 
 }
--- Assignment 1/server.c
+++ Assignment 1/server.c
@@ -21,7 +21,7 @@
 #include <string.h>
 
 void catcher(int sig);
-int newfd;
+int user1, user2;
 
 int main(int argc, const char * argv[]) {
     // packet structure
@@ -49,7 +49,7 @@ int main(int argc, const char * argv[]) {
 
     // bind socket to a port
     srv.sin_family = AF_INET; // use Internet addr family
-    srv.sin_port = htons(1989); // bind socket to port 1989
+    srv.sin_port = htons(53943); // bind socket to port 1989
     srv.sin_addr.s_addr = htonl(INADDR_ANY); // client can connect to any address
     if (bind(sock, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
         perror("bind call failed");
@@ -64,36 +64,84 @@ int main(int argc, const char * argv[]) {
 
     // loop looking for messages
     while (1) {
-        // accept connection
-        newfd = accept(sock, (struct sockaddr*) &cli, &cli_len);
-        if (newfd < 0) {
+        // accept connection from user 1
+        if ((user1 = accept(sock, (struct sockaddr*) &cli, &cli_len) < 0)) {
             perror("accept call failed");
             exit(1);
         }
-        // create child to deal with connection
-        if (fork() == 0) {
-            // receive messages
-            while (recv(newfd, &input, 512, 0) > 0) {
-                send(newfd, &input, 512, 0);
-                // if (input.verb == 1) { // login request
-                //     strcpy(output.source, "Server");
-                //     strcpy(output.destination, input.source);
-                //     strcpy(output.data, strcat("Welcome ", output.destination));
-                //     output.number = 1;
-                //     output.version = 1;
-                //     output.verb = 1;
-                //     send(newfd, &output, 512, 0);
-                // }
-                // if (input.verb == 2) { // message to all
-                //     send(newfd, &input, 512, 0);
-                // }
-            }
-            // when client stops sending, close socket and child
-            close(newfd);
-            exit(0);
+        printf("User 1 has connected. Waiting for second user...\n");
+        
+        // accept connection from user 2
+        if ((user2 = accept(sock, (struct sockaddr*) &cli, &cli_len)) < 0) {
+            perror("accept call failed");
+            exit(1);
         }
-        else
-            close(newfd);
+        printf("User 2 has connected. Ready to send messages!\n");
+
+        if (fork() == 0) {
+            while (1)
+            {
+                while (recv(user1, &input, 512, 0) > 0) {
+                    send(user2, &input, 512, 0);
+                }
+                while (recv(user2, &input, 512, 0))
+                {
+                    send(user1, &input, 512, 0);
+                }
+            }
+        }
+        else {
+            close(user1);
+            close(user2);
+        }
+
+        // create children to deal with connections
+        // if (fork() == 0) { // user 1
+        //     // receive messages
+        //     while (read(user1, &input, 512) > 0) {
+        //         output.version = 1;
+        //         if (input.verb == 1) { // login request
+        //             strcpy(output.source, "Server");
+        //             strcpy(output.destination, input.source);
+        //             strcpy(output.data, strcat("Welcome ", input.source));
+        //             write(user1, &output, 512);
+        //         }
+        //         else if (input.verb == 5) {
+        //             break;
+        //         }
+        //         else { // message to all
+        //             write(user2, &input, 512);
+        //         }
+        //     }
+        //     // when client stops sending, close socket and child
+        //     close(user1);
+        //     exit(0);
+        // }
+        // else if (fork() == 0) { // user 2
+        //     // receive messages
+        //     while (read(user2, &input, 512) > 0) {
+        //         output.version = 1;
+        //         if (input.verb == 1) { // login request
+        //             strcpy(output.source, "Server");
+        //             strcpy(output.destination, input.source);
+        //             strcpy(output.data, strcat("Welcome ", input.source));
+        //             write(user2, &output, 512);
+        //         }
+        //         else if (input.verb == 5) {
+        //             break;
+        //         }
+        //         else { // message to all
+        //             write(user1, &input, 512);
+        //         }
+        //     }
+        //     // when client stops sending, close socket and child
+        //     close(user2);
+        //     exit(0);
+        // }
+        // else {
+        //     close(user1);
+        //     close(user2);
+        // }
     }
     return 0;
 }
@@ -101,6 +149,7 @@ int main(int argc, const char * argv[]) {
 // in case socket becomes disconnected
 void catcher(int sig) {
     signal(SIGPIPE, catcher);
-    close(newfd);
+    close(user1);
+    close(user2);
     exit(0);
 }
