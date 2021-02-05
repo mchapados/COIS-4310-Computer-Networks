f51f01f15a35298e512c911e5b3416a9bf257e30
Sabrina Chapados <takara@Sabrinas-MacBook-Pro.local>
2021-02-04 20:51:01 -0500

    added additional features, everything works

--- Assignment 1/client.c
+++ Assignment 1/client.c
@@ -19,36 +19,35 @@
 #include <signal.h>
 #include <ctype.h>
 #include <string.h>
+#define PACKET_SIZE 512
 
 int main(int argc, const char * argv[]) {
     // packet structure
     struct packet {
-        int number;
-        int version;
-        char source[50];
-        char destination[50];
-        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who 5=quit
-        char data[256];
+        int number; // packet number
+        int version; // 1=Assignment 1, 2=Assignment 2, etc.
+        char source[50]; // username sending the message
+        char destination[50]; // receiver of message (all or username)
+        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who, 5=quit, 6=wait
+        char data[256]; // content of the message
     };
     int sock; // socket descriptor
     struct sockaddr_in srv; // used by connect()
-    struct packet input, output;
-    char username[50];
+    struct packet input, output; // packet to send, packet received from server
+    char username[50], msg_input[307]; // username of this client, string input (destination:message)
     int logged_in = 0;
-    int i;
+    int i; // iterator
 
     // start login request
     printf("Enter a username => ");
     fgets(username, 50, stdin);
-    // remove new line from username
-    for (i = 0; i < 50; i++)
+    for (i = 0; i < 50; i++) // remove new line from username
     {
         if (username[i] == '\n') {
             username[i] = '\0';
             break;
         }
     }
-    printf("Welcome %s\n", username);
 
     // create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -57,8 +56,8 @@ int main(int argc, const char * argv[]) {
     }
 
     // connect to the server
-    srv.sin_family = AF_INET; // use internet address family
-    srv.sin_port = htons(53943); // connect to port 1989
+    srv.sin_family = AF_INET; // use Internet address family
+    srv.sin_port = htons(53943); // connect to port 53943
     srv.sin_addr.s_addr = inet_addr("192.197.151.116"); // connect to loki
     if (connect(sock, (struct sockaddr*) &srv, sizeof(srv)) < 0) {
         perror("connect call failed");
@@ -69,27 +68,54 @@ int main(int argc, const char * argv[]) {
     while (1) {
         input.version = 1;
         strcpy(input.source, username);
+
+        // send login request
+        if (!logged_in) { 
+            input.verb = 1;
+            printf("Logging in...\n");
+            send(sock, &input, PACKET_SIZE, 0);
+            logged_in = 1;
+        }
+        
+        // wait for responce from server
+        do {
+            if (recv(sock, &output, PACKET_SIZE, 0) > 0) {
+                printf("Message from %s:\n  %s\n", output.source, output.data);
+                if (output.verb == 6)
+                    printf("Waiting for messages...\n");
+            }
+            else {
+                printf("Server has died\n");
+                close(sock);
+                exit(1);
+            } 
+        } while (output.verb == 6);
+
+        // get message input
         printf("Send message to: ");
         fgets(input.destination, 50, stdin);
-        if (strncmp(input.destination, "bye", 3) == 0) { // end loop
+
+        // check exit condition
+        if (strncmp(input.destination, "bye", 3) == 0) {
             printf("Sorry to see you go\n");
+            input.verb = 5;
+            // notify server that user has quit
+            send(sock, &input, PACKET_SIZE, 0); 
             exit(0);
         }
-        printf("Message: ");
-        fgets(input.data, 256, stdin);
-        if (strncmp(input.destination, "all", 3) == 0)
-            input.verb = 2;
-        else
-            input.verb = 3;
-        printf("Sending message...\n");
-        send(sock, &input, 512, 0);
-        printf("Message sent\n");
-        if (recv(sock, &output, 512, 0) > 0)
-            printf("%s: %s\n", output.source, output.data);
+
+        // determine message type
+        if (strncmp(input.destination, "who", 3) == 0) {
+            input.verb = 4;
+            printf("Sending request to server...\n");
+            send(sock, &input, PACKET_SIZE, 0);
+        }
         else {
-            printf("Server has died\n");
-            close(sock);
-            exit(1);
-        } 
+            input.verb = 2;
+            printf("Message: ");
+            fgets(input.data, 256, stdin);
+            printf("Sending message...\n");
+            send(sock, &input, PACKET_SIZE, 0);
+        }
     }    
 }
--- Assignment 1/server.c
+++ Assignment 1/server.c
@@ -19,26 +19,30 @@
 #include <signal.h>
 #include <ctype.h>
 #include <string.h>
+#define PACKET_SIZE 512
+#define MAX_USERS 2
 
 void catcher(int sig);
-int user1, user2;
+int user0, user1; // clients
+char usernames[MAX_USERS][50]; // array storing usernames of connected clients
 
 int main(int argc, const char * argv[]) {
     // packet structure
     struct packet {
-        int number;
-        int version;
-        char source[50];
-        char destination[50];
-        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who 
-        char data[256];
+        int number; // packet number
+        int version; // 1=Assignment 1, 2=Assignment 2, etc.
+        char source[50]; // username sending the message
+        char destination[50]; // receiver of message (all or username)
+        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who, 5=quit, 6=wait
+        char data[256]; // content of the message
     };
     int sock; // socket descriptor
     struct sockaddr_in srv; // used by bind()
     struct sockaddr_in cli; // used by accept()
     int cli_len = sizeof(cli); // used by accept()
-    struct packet input, output;
+    struct packet input, output; // packet from client, packet to send from server
     int not_done = 1;
+    int i; // iterator
 
     signal(SIGPIPE, catcher);
     
@@ -49,8 +53,8 @@ int main(int argc, const char * argv[]) {
     }
 
     // bind socket to a port
-    srv.sin_family = AF_INET; // use Internet addr family
-    srv.sin_port = htons(53943); // bind socket to port 1989
+    srv.sin_family = AF_INET; // use Internet address family
+    srv.sin_port = htons(53943); // bind socket to port 53943
     srv.sin_addr.s_addr = htonl(INADDR_ANY); // client can connect to any address
     if (bind(sock, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
         perror("bind call failed");
@@ -65,40 +69,145 @@ int main(int argc, const char * argv[]) {
 
     // loop looking for messages
     while (1) {
+        // accept connection from user 0
+        user0 = accept(sock, (struct sockaddr*) &cli, &cli_len);
+        if (user0 < 0) {
+            perror("accept call failed");
+            exit(1);
+        }
+        printf("User 0 has connected. Waiting for second user...\n");
+        
         // accept connection from user 1
         user1 = accept(sock, (struct sockaddr*) &cli, &cli_len);
         if (user1 < 0) {
             perror("accept call failed");
             exit(1);
         }
-        //printf("User 1 has connected. Waiting for second user...\n");
-        
-        // accept connection from user 2
-        user2 = accept(sock, (struct sockaddr*) &cli, &cli_len);
-        if (user2 < 0) {
-            perror("accept call failed");
-            exit(1);
-        }
-        // printf("User 2 has connected. Ready to send messages!\n");
+        printf("User 1 has connected. Ready to send messages!\n");
 
         // create child to deal with connection
         if (fork() == 0) {
+            // output.version = 1;
+            // strcpy(output.source, "Server");
+            // strcpy(output.data, "Please login");
+            // output.verb = 0;
+            // send(user0, &output, PACKET_SIZE, 0);
+            // strcpy(output.data, "Please wait");
+            // output.verb = 6;
+            // send(user1, &output, PACKET_SIZE, 0);
             while (not_done) {
-                if (recv(user1, &input, 512, 0) > 0) 
-                    send(user2, &input, 512, 0);
+                // get message from user 0
+                if (recv(user0, &input, PACKET_SIZE, 0) > 0) {
+                    // login request
+                    if (input.verb == 1) { 
+                        strcpy(usernames[0], input.source); // store username
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, "Welcome ");
+                        strcat(output.data, usernames[0]);
+                        output.verb = 6; // wait for user 1
+                        send(user0, &output, PACKET_SIZE, 0);
+                    }
+                    // user 0 has quit
+                    else if (input.verb == 5) { 
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, usernames[0]);
+                        strcat(output.data, " has quit.");
+                        send(user1, &output, PACKET_SIZE, 0);
+                        break; // end loop
+                    }
+                    // who request
+                    else if (input.verb == 4) {
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, "Connected users: ");
+                        // get list of usernames
+                        for (i = 0; i < MAX_USERS; i++) {
+                            strcat(output.data, usernames[i]);
+                            if (i != MAX_USERS-1)
+                                strcat(output.data, ", ");
+                        }
+                        output.verb = 6;
+                        send(user0, &output, PACKET_SIZE, 0);
+                        strcpy(output.data, "Please send a message.");
+                        output.verb = 0;
+                        send(user1, &output, PACKET_SIZE, 0);
+                    }
+                    // message to all
+                    else {
+                        // relay message
+                        strcpy(output.source, usernames[0]);
+                        strcpy(output.data, input.data);
+                        output.verb = 2;
+                        send(user1, &output, PACKET_SIZE, 0);
+                        // tell user 0 to wait
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, "Message sent.");
+                        output.verb = 6;
+                        send(user0, &output, PACKET_SIZE, 0);
+                    }
+                }
                 else
                     not_done = 0;
-                if (recv(user2, &input, 512, 0) > 0) 
-                    send(user1, &input, 512, 0);
+                
+                // get message from user 1
+                if (recv(user1, &input, PACKET_SIZE, 0) > 0) {
+                    // login request
+                    if (input.verb == 1) { 
+                        strcpy(usernames[1], input.source); // store username
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, "Welcome ");
+                        strcat(output.data, usernames[1]);
+                        output.verb = 6; // wait for message from user 1
+                        send(user1, &output, PACKET_SIZE, 0);
+                        output.verb = 0; // tell user 0 to stop waiting
+                        send(user0, &output, PACKET_SIZE, 0);
+                    }
+                    // user 1 has quit
+                    else if (input.verb == 5) { 
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, usernames[1]);
+                        strcat(output.data, " has quit.");
+                        send(user0, &output, PACKET_SIZE, 0);
+                        break; // end loop
+                    }
+                    // who request
+                    else if (input.verb == 4) {
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, "Connected users: ");
+                        // get list of usernames
+                        for (i = 0; i < MAX_USERS; i++) {
+                            strcat(output.data, usernames[i]);
+                            if (i != MAX_USERS-1)
+                                strcat(output.data, ", ");
+                        }
+                        output.verb = 6;
+                        send(user1, &output, PACKET_SIZE, 0);
+                        strcpy(output.data, "Please send a message.");
+                        output.verb = 0;
+                        send(user0, &output, PACKET_SIZE, 0);
+                    }
+                    // message to all
+                    else {
+                        // relay message
+                        strcpy(output.source, usernames[1]);
+                        strcpy(output.data, input.data);
+                        output.verb = 2;
+                        send(user0, &output, PACKET_SIZE, 0);
+                        // tell user 0 to wait
+                        strcpy(output.source, "Server");
+                        strcpy(output.data, "Message sent.");
+                        output.verb = 6;
+                        send(user1, &output, PACKET_SIZE, 0);
+                    }
+                }
                 else
                     not_done = 0;
             }
+            close(user0);
             close(user1);
-            close(user2);
         }
         else {
+            close(user0);
             close(user1);
-            close(user2);
         }
     }
     return 0;
@@ -107,7 +216,7 @@ int main(int argc, const char * argv[]) {
 // in case socket becomes disconnected
 void catcher(int sig) {
     signal(SIGPIPE, catcher);
+    close(user0);
     close(user1);
-    close(user2);
     exit(0);
 }
