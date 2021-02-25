44e7c1e013b2fd61093b7190e70d690a01183003
Sabrina Chapados <takara@Sabrinas-MacBook-Pro.local>
2021-02-25 02:47:05 -0500

    Cleaned up code and added checksum feature, not yet tested

--- Assignment 2/client.c
+++ Assignment 2/client.c
@@ -1,15 +1,16 @@
-/*
-  
- COIS 4310H ASSIGNMENT #1 - CHAT APPLICATION
- FILENAME: client.c
+/* --------------------------------------------------------------------------------------------
+    COIS 4310H ASSIGNMENT #2 - CHAT APPLICATION v2.0
+    FILENAME: client.c
 
- WRITTEN BY: Sabrina Chapados - Jan-Feb, 2021
- 
- PURPOSE: Runs a client, which connects to the server to allow for two-way communication with a second client. 
- USAGE: ./client
- PARAMETERS: NONE
- 
-*/
+    WRITTEN BY: S. Chapados - Feb, 2021
+    
+    PROGRAM DESCRIPTION:
+    Runs a client, which connects to the server to allow for two-way communication with a 
+    second client. 
+    
+    USAGE: ./client
+    PARAMETERS: NONE
+   -------------------------------------------------------------------------------------------- */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,30 +19,36 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <string.h>
+#include <time.h>
 #define PACKET_SIZE 512
 
+// FUNCTIONS -----------------------------------------------------------------------------------
+int CheckSum(char msg[], int corruptible);
+
+// MAIN ----------------------------------------------------------------------------------------
 int main(int argc, const char * argv[]) {
-    // packet structure
-    struct packet {
+    struct packet { // PACKET STRUCTURE
         int number; // packet number
         int version; // 1=Assignment 1, 2=Assignment 2, etc.
-        char source[50]; // username sending the message
-        char destination[50]; // receiver of message (all, username, who, or bye)
-        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who, 5=quit, 6=wait
+        char source[25]; // username sending the message
+        char destination[25]; // receiver of message (all, username, who, or bye)
+        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who, 5=quit, 6=wait, 7=NAK
+        int checksum; // used for checking that data field is not corrupt
         char data[256]; // content of the message
     };
     int sock; // socket descriptor
     struct sockaddr_in srv; // used by connect()
     struct packet input, output; // packet to send, packet received from server
-    char username[50]; // username of this client
-    int logged_in = 0;
+    char username[25]; // username of this client
+    int logged_in = 0, packet_count = 0;
     int i; // iterator
 
     // start login request
-    printf("Enter a username => ");
-    fgets(username, 50, stdin);
-    for (i = 0; i < 50; i++) // remove new line from username
-    {
+    printf("----------------------------------------------------------------------\n");
+    printf(" Welcome to the chat application!\n");
+    printf(" Please enter a username (max 25 characters) => ");
+    fgets(username, 25, stdin);
+    for (i = 0; i < 25; i++) { // remove new line from username
         if (username[i] == '\n') {
             username[i] = '\0';
             break;
@@ -63,14 +70,28 @@ int main(int argc, const char * argv[]) {
         exit(1);
     }
 
+    // initialize packet header
+    input.version = 2;
+    strcpy(input.source, username);
+
+    // show instructions for users
+    printf("----------------------------------------------------------------------\n"
+        "                             INSTRUCTIONS\n"
+        "----------------------------------------------------------------------\n"
+        " Enter commands in the TO field (commands must be lower case)\n"  
+        "   - DISCONNECT: bye\n"
+        "   - GET LIST OF CONNECTED USERS: who\n"
+        "   - SEND MESSAGE TO ALL USERS: all\n"
+        "   - SEND PRIVATE MESSAGE: recipient username\n"
+        "----------------------------------------------------------------------\n");
+
     // send and receive messages
     while (1) {
-        input.version = 1;
-        strcpy(input.source, username);
-
         // send login request
         if (!logged_in) { 
             input.verb = 1;
+            input.number = ++packet_count; // this should be packet #1
+            input.checksum = 0;
             printf("Logging in...\n");
             send(sock, &input, PACKET_SIZE, 0);
             logged_in = 1;
@@ -80,19 +101,28 @@ int main(int argc, const char * argv[]) {
         do {
             if (recv(sock, &output, PACKET_SIZE, 0) > 0) {
                 printf("Message from %s:\n  %s\n", output.source, output.data);
-                if (output.verb == 6)
-                    printf("Waiting for messages...\n");
+                if (output.verb == 6) // wait
+                    printf("\nWaiting for messages...\n");
+                // NAK - resend last packet
+                else if (output.verb == 7) { 
+                    printf("Re-sending last message...\n");
+                    input.checksum = CheckSum(input.data, 0); // not corruptible this time
+                    send(sock, &input, PACKET_SIZE, 0);
+                }
             }
             else {
                 printf("Server has died\n");
                 close(sock);
                 exit(1);
             } 
-        } while (output.verb == 6);
+        } while (output.verb == 6 || output.verb == 7);
 
-        // get message input
+        // get message destination input from user
         printf("Send message to: ");
-        fgets(input.destination, 50, stdin);
+        fgets(input.destination, 25, stdin);
+
+        // increment packet count and assign to number
+        input.number = ++packet_count;
 
         // check exit condition
         if (strncmp(input.destination, "bye", 3) == 0) {
@@ -104,17 +134,41 @@ int main(int argc, const char * argv[]) {
         }
 
         // determine message type
-        if (strncmp(input.destination, "who", 3) == 0) {
+        if (strncmp(input.destination, "who", 3) == 0) { // who request
             input.verb = 4;
+            input.checksum = 0; 
             printf("Sending request to server...\n");
             send(sock, &input, PACKET_SIZE, 0);
         }
-        else {
-            input.verb = 2;
+        else { // regular message
+            input.verb = (strncmp(input.destination, "all", 3) == 0) ? 2 : 3;
+            // get message input from user
             printf("Message: ");
             fgets(input.data, 256, stdin);
+            input.checksum = CheckSum(input.data, 1); // can be corrupted
             printf("Sending message...\n");
             send(sock, &input, PACKET_SIZE, 0);
         }
-    }    
+    } // end while (message loop)    
+} // end main
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: CheckSum
+    DESCRIPTION: Used for packet validation. Sums up the ASCII values of characters in a string
+    and returns the result.
+    PARAMETERS:
+        msg char[] containing string to create checksum from
+        corruptible int (bool) if true, 10% chance of incorrect return value 
+    RETURNS: int
+*/
+int CheckSum(char msg[], int corruptible) {
+    int sum = 0, i;
+    if (corruptible) {
+        srand(time(0)); // seed for random number
+        if ((rand() % 10) == 7) // 10% chance of corrupting the packet
+            return 1984;
+    }
+    for (i = 0; i < strlen(msg); ++i) 
+        sum += (int) msg[i];
+    return sum;
 }
--- Assignment 2/server.c
+++ Assignment 2/server.c
@@ -1,15 +1,15 @@
-/*
-  
- COIS 4310H ASSIGNMENT #1 - CHAT APPLICATION
- FILENAME: server.c
+/* --------------------------------------------------------------------------------------------
+    COIS 4310H ASSIGNMENT #2 - CHAT APPLICATION v2.0
+    FILENAME: server.c
 
- WRITTEN BY: Sabrina Chapados - Jan-Feb, 2021
- 
- PURPOSE: Runs a chat server using sockets for communication between two clients.
- USAGE: ./server &
- PARAMETERS: NONE
-
-*/
+    WRITTEN BY: S. Chapados - Feb, 2021
+    
+    PROGRAM DESCRIPTION:
+    Runs a chat server using Internet sockets for communication between two clients.
+    
+    USAGE: ./server &
+    PARAMETERS: NONE
+   -------------------------------------------------------------------------------------------- */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,28 +21,36 @@
 #define PACKET_SIZE 512
 #define MAX_USERS 2
 
+// FUNCTIONS -----------------------------------------------------------------------------------
 void catcher(int sig);
+int ConnectUser();
+int GetMessage(int user);
+struct packet Login(int user, struct packet in);
+void Quit(int user);
+void Who(int user);
+struct packet Message(int from, struct packet in);
+int CheckSum(char msg[], int value);
+
+// GLOBAL VARIABLES ----------------------------------------------------------------------------
 int user0, user1; // clients
-char usernames[MAX_USERS][50]; // array storing usernames of connected clients
+char usernames[MAX_USERS][25]; // array storing usernames of connected clients
+struct packet { // PACKET STRUCTURE
+    int number; // packet number
+    int version; // 1=Assignment 1, 2=Assignment 2, etc.
+    char source[25]; // username sending the message
+    char destination[25]; // receiver of message (all or username)
+    int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who, 5=quit, 6=wait, 7=NAK
+    int checksum; // used for checking that data field is not corrupt
+    char data[256]; // content of the message
+};
+int sock; // socket descriptor
+struct sockaddr_in srv; // used by bind()
+struct sockaddr_in cli; // used by accept()
+int cli_len = sizeof(cli); // used by accept()
 
+// MAIN ----------------------------------------------------------------------------------------
 int main(int argc, const char * argv[]) {
-    // packet structure
-    struct packet {
-        int number; // packet number
-        int version; // 1=Assignment 1, 2=Assignment 2, etc.
-        char source[50]; // username sending the message
-        char destination[50]; // receiver of message (all or username)
-        int verb; // 1=login, 2=messageAll, 3=privateMessage, 4=who, 5=quit, 6=wait
-        char data[256]; // content of the message
-    };
-    int sock; // socket descriptor
-    struct sockaddr_in srv; // used by bind()
-    struct sockaddr_in cli; // used by accept()
-    int cli_len = sizeof(cli); // used by accept()
-    struct packet input, output; // packet from client, packet to send from server
     int not_done = 1;
-    int i; // iterator
-
     signal(SIGPIPE, catcher);
     
     // create socket
@@ -69,152 +77,232 @@ int main(int argc, const char * argv[]) {
     // loop looking for messages
     while (1) {
         // accept connection from user 0
-        user0 = accept(sock, (struct sockaddr*) &cli, &cli_len);
-        if (user0 < 0) {
-            perror("accept call failed");
-            exit(1);
-        }
+        user0 = ConnectUser();
         printf("User 0 has connected. Waiting for second user...\n");
         
         // accept connection from user 1
-        user1 = accept(sock, (struct sockaddr*) &cli, &cli_len);
-        if (user1 < 0) {
-            perror("accept call failed");
-            exit(1);
-        }
+        user1 = ConnectUser();
         printf("User 1 has connected. Ready to send messages!\n");
 
         // create child to deal with connection
         if (fork() == 0) {
             while (not_done) {
-                output.version = 1;
-                // get message from user 0
-                if (recv(user0, &input, PACKET_SIZE, 0) > 0) {
-                    printf("Received message from User 0: %s\nVerb: %d\n", usernames[0], input.verb);
-                    // login request
-                    if (input.verb == 1) { 
-                        strcpy(usernames[0], input.source); // store username
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, "Welcome ");
-                        strcat(output.data, usernames[0]);
-                        output.verb = 6; // wait for user 1
-                        send(user0, &output, PACKET_SIZE, 0);
-                    }
-                    // user 0 has quit
-                    else if (input.verb == 5) { 
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, usernames[0]);
-                        strcat(output.data, " has quit.");
-                        send(user1, &output, PACKET_SIZE, 0);
-                        break; // end loop
-                    }
-                    // who request
-                    else if (input.verb == 4) {
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, "Connected users: ");
-                        // get list of usernames
-                        for (i = 0; i < MAX_USERS; i++) {
-                            strcat(output.data, usernames[i]);
-                            if (i != MAX_USERS-1)
-                                strcat(output.data, ", ");
-                        }
-                        output.verb = 6;
-                        send(user0, &output, PACKET_SIZE, 0);
-                        strcpy(output.data, "Please send a message.");
-                        output.verb = 0;
-                        send(user1, &output, PACKET_SIZE, 0);
-                    }
-                    // message to all
-                    else {
-                        // relay message
-                        strcpy(output.source, usernames[0]);
-                        strcpy(output.data, input.data);
-                        output.verb = 2;
-                        send(user1, &output, PACKET_SIZE, 0);
-                        // tell user 0 to wait
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, "Message sent.");
-                        output.verb = 6;
-                        send(user0, &output, PACKET_SIZE, 0);
-                    }
-                }
-                else {
-                    printf("User 0 has stopped sending\n");
+                if (!GetMessage(0)) // get message from user 0
                     not_done = 0;
-                }
-
-                // get message from user 1
-                if (recv(user1, &input, PACKET_SIZE, 0) > 0) {
-                    printf("Received message from User 1: %s\nVerb: %d\n", usernames[1], input.verb);
-                    // login request
-                    if (input.verb == 1) { 
-                        strcpy(usernames[1], input.source); // store username
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, "Welcome ");
-                        strcat(output.data, usernames[1]);
-                        output.verb = 6; // wait for message from user 1
-                        send(user1, &output, PACKET_SIZE, 0);
-                        output.verb = 0; // tell user 0 to stop waiting
-                        send(user0, &output, PACKET_SIZE, 0);
-                    }
-                    // user 1 has quit
-                    else if (input.verb == 5) { 
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, usernames[1]);
-                        strcat(output.data, " has quit.");
-                        send(user0, &output, PACKET_SIZE, 0);
-                        break; // end loop
-                    }
-                    // who request
-                    else if (input.verb == 4) {
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, "Connected users: ");
-                        // get list of usernames
-                        for (i = 0; i < MAX_USERS; i++) {
-                            strcat(output.data, usernames[i]);
-                            if (i != MAX_USERS-1)
-                                strcat(output.data, ", ");
-                        }
-                        output.verb = 6;
-                        send(user1, &output, PACKET_SIZE, 0);
-                        strcpy(output.data, "Please send a message.");
-                        output.verb = 0;
-                        send(user0, &output, PACKET_SIZE, 0);
-                    }
-                    // message to all
-                    else {
-                        // relay message
-                        strcpy(output.source, usernames[1]);
-                        strcpy(output.data, input.data);
-                        output.verb = 2;
-                        send(user0, &output, PACKET_SIZE, 0);
-                        // tell user 0 to wait
-                        strcpy(output.source, "Server");
-                        strcpy(output.data, "Message sent.");
-                        output.verb = 6;
-                        send(user1, &output, PACKET_SIZE, 0);
-                    }
-                }
-                else {
-                    printf("User 1 has stopped sending\n");
+                if (!GetMessage(1)) // get message from user 1
                     not_done = 0;
-                }
             }
             close(user0);
             close(user1);
         }
-        else {
+        else { // parent process
             close(user0);
             close(user1);
         }
     }
-    return 0;
-}
+    exit(0);
+} // end main
 
-// in case socket becomes disconnected
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: catcher
+    DESCRIPTION: Used in case socket becomes disconnected.
+    RETURNS: VOID
+*/
 void catcher(int sig) {
     signal(SIGPIPE, catcher);
     close(user0);
     close(user1);
     exit(0);
+}
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: ConnectUser
+    DESCRIPTION: Accepts a connection from a client. If successful, returns the value of the
+    accept() function.
+    RETURNS: int
+*/
+int ConnectUser() {
+    int user = accept(sock, (struct sockaddr*) &cli, &cli_len);
+    if (user < 0) {
+        perror("accept call failed");
+        exit(1);
+    }
+    return user;
+}
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: GetMessage
+    DESCRIPTION: Used to send and receive messages. Returns false if recv() fails.
+    PARAMETERS:
+        user int containing the user number to get a message from
+    RETURNS: int (bool)
+*/
+int GetMessage(int user) {
+    struct packet input, output; // packet from client, packet to send from server
+    int user_socket = (user == 0) ? user0 : user1;
+    if (recv(user_socket, &input, PACKET_SIZE, 0) > 0) {
+        printf("Received packet %d with verb %d from User: %s\n", input.number, input.verb, usernames[user]);
+        switch (input.verb) {
+            case 1: // login request
+                output = Login(user, input);
+                send(user_socket, &output, PACKET_SIZE, 0);
+                break;
+            case 5: // user has disconnected
+                Quit(0);
+                break;
+            case 4: // who request
+                Who(0);
+                break;
+            case 2: case 3: // regular message
+                output = Message(user, input);
+                send(user_socket, &output, PACKET_SIZE, 0);
+                if (output.verb == 7)
+                    GetMessage(user); // loop until packet is successfully received
+                break;
+            default:
+                break;
+        } // end switch
+    }
+    else {
+        printf("User %d has stopped sending\n", user);
+        return 0;
+    }
+    return 1;
+}
+
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: Login
+    DESCRIPTION: Processes login requests. Saves the client's username and returns an ACK 
+    packet from the server.
+    PARAMETERS:
+        user int containing the user number
+        in packet containing the message received from the user
+    RETURNS: packet
+*/
+struct packet Login(int user, struct packet in) {
+    struct packet out;
+    char msg[] = "Welcome ";
+    out.version = 2;
+    strcpy(usernames[user], in.source); // store username
+    strcat(msg, usernames[user]); // create welcome message
+    strcpy(out.source, "Server");
+    strcpy(out.data, msg);
+    out.verb = 6; // wait for next user
+    return out;
+}
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: Quit
+    DESCRIPTION: Called when a user has disconnected. Ends the chat session.
+    PARAMETERS:
+        user int containing the user number who has left
+    RETURNS: packet
+*/
+void Quit(int user) {
+    struct packet out;
+    out.version = 2;
+    char msg[] = usernames[user];
+    strcat(msg, " has quit.");
+    strcpy(out.source, "Server");
+    strcpy(out.data, msg);
+    if (user == 0) // notify other user
+        send(user1, &out, PACKET_SIZE, 0);
+    else
+        send(user0, &out, PACKET_SIZE, 0);
+    // close sockets
+    close(user0);
+    close(user1);
+    exit(0);
+}
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: Who
+    DESCRIPTION: Processes who requests. Returns packet containing the list of connected users.
+    PARAMETERS:
+        user int containing the requesting user number
+    RETURNS: packet
+*/
+void Who(int user) {
+    struct packet out;
+    int i;
+    out.version = 2;
+    strcpy(out.source, "Server");
+    strcpy(out.data, "Connected users: ");
+    for (i = 0; i < MAX_USERS; i++) { // get list of usernames
+        strcat(out.data, usernames[i]);
+        if (i != MAX_USERS-1)
+            strcat(out.data, ", ");
+    }
+    out.verb = 6; // tell user to wait
+
+    // send response
+    if (user == 0) 
+        send(user0, &out, PACKET_SIZE, 0);
+    else
+        send(user1, &out, PACKET_SIZE, 0);
+
+    // tell other user to stop waiting
+    strcpy(out.data, "Please send a message.\n");
+    out.verb = 0;
+    if (user == 1) 
+        send(user0, &out, PACKET_SIZE, 0);
+    else
+        send(user1, &out, PACKET_SIZE, 0);
+}
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: Message
+    DESCRIPTION: 
+    PARAMETERS:
+        from int containing the user number of the sender
+        in packet containing the message received from the user
+    RETURNS: packet
+*/
+struct packet Message(int from, struct packet in) {
+    int to = (from == 0) ? user1 : user0; // recipient
+    struct packet out;
+    out.version = 2;
+    
+    if (CheckSum(in.data, in.checksum) > 0) { // message is not corrupted
+        // relay message to other user
+        strcpy(out.source, usernames[from]);
+        if (in.verb == 3) {
+            strcpy(out.data, "(private message) ");
+            strcat(out.data, in.data);
+        }
+        else
+            strcpy(out.data, in.data);
+        out.verb = in.verb;
+        send(to, &out, PACKET_SIZE, 0);
+        // tell sender to wait
+        strcpy(out.source, "Server");
+        strcpy(out.data, "Message sent.");
+        out.verb = 6;
+    }
+    else { // packet was corrupted
+        printf("Packet number %d is corrupted.\n", in.number);
+        strcpy(out.source, "Server");
+        strcpy(out.data, "Message not received. Please try again.");
+        out.verb = 7;
+    }
+    // send ACK or NAK back to client
+    return out;
+}
+
+/*  --------------------------------------------------------------------------------------------
+    FUNCTION: CheckSum
+    DESCRIPTION: Used for packet validation. Sums up the ASCII values of characters in a string
+    and returns true if the value is correct.
+    PARAMETERS:
+        msg char[] containing string to check
+        value int containing the value that sum should be equal to
+    RETURNS: int (bool)
+*/
+int CheckSum(char msg[], int value) {
+    int sum = 0, i;
+    for (i = 0; i < strlen(msg); ++i) // calculate the sum 
+        sum += (int) msg[i];
+    // return true if value is equal to sum
+    return (value == sum) ? 1 : 0;
 }
